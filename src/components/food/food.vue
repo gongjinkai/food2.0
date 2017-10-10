d4648334c9d5d6cfbe93cce060981086c9aa930e
author <498752819@qq.com>
2017-10-11 00:36:16 +0800

    'ratings'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,15 +18,25 @@
             <span class="now">￥{{food.price}}</span>
             <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
-        </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food" @add="addFood"></cartcontrol>
-        </div>
-        <transition name="fade">
-          <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
-            加入购物车
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food" @add="addFood"></cartcontrol>
           </div>
-        </transition>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
+              加入购物车
+            </div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -35,6 +45,13 @@
   import BScroll from 'better-scroll'
   import Vue from 'vue'
   import cartcontrol from '../../components/cartcontrol/cartcontrol'
+  import split from '../../components/split/split'
+  import ratingselect from '../../components/ratingselect/ratingselect'
+
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
+
   export default {
     props: {
       food: {
@@ -43,12 +60,21 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
@@ -74,7 +100,9 @@
       }
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
     }
   }
 </script>
@@ -114,6 +142,7 @@
         font-size 20px
         color #fff
   .content
+    position relative
     padding 18px
     .title
       line-height 14px
@@ -142,7 +171,6 @@
         text-decoration line-through
         font-size 10px
         color: rgb(147, 153, 159)
-
   .cartcontrol-wrapper
     position absolute
     right 12px
@@ -166,4 +194,23 @@
     &.fade-enter, &.fade-leave-active
       opacity: 0
       z-index: -1
+  .info
+    padding 18px
+    .title
+      line-height 14px
+      margin-bottom 6px
+      font-size 14px
+      color rgb(7,17,27)
+    .text
+      line-height 24px
+      padding 0 8px
+      font-size 12px
+      color rgb(77,85,93)
+  .rating
+    padding-top 18px
+    .title
+      line-height 14px
+      margin-left 18px
+      font-size 14px
+      color rgb(7,17,27)
 </style>
