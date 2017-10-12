d91b6bda85e8be65ae4730e27f90b94f4cd41cab
author <498752819@qq.com>
2017-10-13 00:48:29 +0800

    'date'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -45,7 +45,7 @@
                   <span class="name">{{rating.username}}</span>
                   <img class="avatar" width="12" height="12" :src="rating.avatar">
                 </div>
-                <div class="time">{{rating.rateTime}}</div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
                 <p class="text">
                   <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
                 </p>
@@ -61,6 +61,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
   import Vue from 'vue'
+  import {formatDate} from '../../common/js/date'
   import cartcontrol from '../../components/cartcontrol/cartcontrol'
   import split from '../../components/split/split'
   import ratingselect from '../../components/ratingselect/ratingselect'
@@ -137,6 +138,12 @@
         this.$nextTick(() => {
           this.scroll.refresh();
         });
+      }
+    },
+    filters: {
+      formatDate(time){
+        let date = new Date(time);
+        return formatDate(date,'yyyy-MM-dd hh:mm');
       }
     },
     components: {
@@ -290,4 +297,8 @@
             color rgb(0,160,220)
           .icon-thumb_down
             color rgb(147,153,159)
+      .no-rating
+        padding 16px 0
+        font-size 12px
+        color rgb(147,153,159)
 </style>
