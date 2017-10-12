2feb5581295ba9f75ba85a4708e794f36802c881
author <498752819@qq.com>
2017-10-13 00:24:32 +0800

    'add'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -40,7 +40,7 @@
                         :ratings="food.ratings"></ratingselect>
           <div class="rating-wrapper">
             <ul v-show="food.ratings && food.ratings.length">
-              <li v-for="rating in food.ratings" class="rating-item border-1px">
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
                 <div class="user">
                   <span class="name">{{rating.username}}</span>
                   <img class="avatar" width="12" height="12" :src="rating.avatar">
@@ -111,6 +111,17 @@
         }
         this.$emit('add', event.target);
         Vue.set(this.food, 'count', 1)
+      },
+      needShow(type,text) {
+        if(this.onlyContent && !text){
+          return false
+        }
+        if(this.selectType === ALL){
+          return true
+        }
+        else{
+          return type === this.selectType;
+        }
       },
       addFood() {
         this.$emit('add', event.target);
