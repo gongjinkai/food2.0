29dd491d3c724b51a9f0abc39c5999e5028a3cdb
author <498752819@qq.com>
2017-10-18 00:09:52 +0800

    'commit'

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="seller">
-    i am seller
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc">
+          <star :size="36" score="seller.score"></star>
+          <span class="text">{{seller.ratingCount}}</span>
+          <span class="text">{{seller.sellCount}}</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>
+            </div>
+          </li>
+          <li>
+            <h2>商家配送</h2>
+            
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
-<script type="text/ecmascript-6"></script>
+<script type="text/ecmascript-6">
+  import star from '../../components/star/star'
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    components: {
+      star
+    }
+  }
+</script>
 <style  lang="stylus" rel="stylesheet/stylus">
 
 </style>
