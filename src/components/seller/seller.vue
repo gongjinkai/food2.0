395e396fa118ff134a70902449072e98d430bf80
author <498752819@qq.com>
2017-10-19 00:26:56 +0800

    默认的

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -46,5 +46,32 @@
   }
 </script>
 <style  lang="stylus" rel="stylesheet/stylus">
-
+  .seller
+    position absolute
+    top 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
+    .overview
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        color rgb(7,17,27)
+        font-size 14px
+      .desc
+        padding-bottom 18px
+        line-height 18px
+        border-1px(rgba(7,17,27,0.1))
+        font-size 0
+        .star
+          display inline-block
+          margin-right 8px
+          vertical-align top
+        .text
+          margin-right 12px
+          display inline-block
+          vertical-align top
+          font-size 10px
 </style>
