d651564200fd74975c9e5eb71f09c8dd1971156f
author <498752819@qq.com>
2017-10-19 00:17:14 +0800

    默认的

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -4,20 +4,28 @@
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
         <div class="desc">
-          <star :size="36" score="seller.score"></star>
-          <span class="text">{{seller.ratingCount}}</span>
-          <span class="text">{{seller.sellCount}}</span>
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
         </div>
         <ul class="remark">
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>
+              <span class="stress">{{seller.minPrice}}</span>元
             </div>
           </li>
-          <li>
+          <li class="block">
             <h2>商家配送</h2>
-
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
           </li>
         </ul>
       </div>
